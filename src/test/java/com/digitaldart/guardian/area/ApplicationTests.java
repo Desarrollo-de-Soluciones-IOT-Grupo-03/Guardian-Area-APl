89b4019eddc1d891fa7f7d19e372e5a950c82d30
Cristian Quito <cristianandresquito@gmail.com>
2024-11-01 19:29:09 -0500

    initial commit

---
+++ src/test/java/com/digitaldart/guardian/area/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitaldart.guardian.area;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
