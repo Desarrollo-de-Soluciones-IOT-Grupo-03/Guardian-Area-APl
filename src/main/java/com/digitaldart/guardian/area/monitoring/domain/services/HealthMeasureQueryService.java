aba40f88c1e31cfcc5ba4211909da67bfbbe54a2
MaryJenn05 <u202120911@upc.edu.pe>
2024-11-18 22:06:39 -0500

    feat: update HealthMeasure Query Service and repository

--- src/main/java/com/digitaldart/guardian/area/monitoring/domain/services/HealthMeasureQueryService.java
+++ src/main/java/com/digitaldart/guardian/area/monitoring/domain/services/HealthMeasureQueryService.java
@@ -4,7 +4,6 @@ import com.digitaldart.guardian.area.monitoring.domain.model.aggregates.HealthMe
 import com.digitaldart.guardian.area.monitoring.domain.model.queries.GetAllHealthMeasuresByDeviceRecordIdQuery;
 import com.digitaldart.guardian.area.monitoring.domain.model.queries.GetHealthMeasuresDailyAverageFromCurrentMonthByGuardianAreaDeviceRecordIdQuery;
 import com.digitaldart.guardian.area.monitoring.domain.model.valueobjects.HealthMeasureDailyAverage;
-
 import java.util.List;
 
 public interface HealthMeasureQueryService {
