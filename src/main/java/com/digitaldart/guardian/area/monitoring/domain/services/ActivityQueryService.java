1d3d3f50a691814ff7973090eb4277c5afc8e1c4
steve <stev8855@gmail.com>
2024-11-18 23:09:00 -0500

    feat: Add ApiKey value object to DeviceCommandService and update ActivityQueryService interface

--- src/main/java/com/digitaldart/guardian/area/monitoring/domain/services/ActivityQueryService.java
+++ src/main/java/com/digitaldart/guardian/area/monitoring/domain/services/ActivityQueryService.java
@@ -3,6 +3,7 @@ package com.digitaldart.guardian.area.monitoring.domain.services;
 import com.digitaldart.guardian.area.monitoring.domain.model.aggregates.Activity;
 import com.digitaldart.guardian.area.monitoring.domain.model.queries.GetAllActivitiesByGuardianAreaDeviceRecordId;
 import com.digitaldart.guardian.area.monitoring.domain.model.queries.GetAllActivitiesByGuardianAreaDeviceRecordIdAndActivityType;
+
 import java.util.List;
 
 public interface ActivityQueryService {
