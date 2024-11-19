34933d9587b1fbc22a105a46a08da7c91580559d
steve <stev8855@gmail.com>
2024-11-18 23:09:22 -0500

    feat: Add missing import statement in HealthMeasureQueryService for improved functionality

--- src/main/java/com/digitaldart/guardian/area/monitoring/domain/services/HealthMeasureQueryService.java
+++ src/main/java/com/digitaldart/guardian/area/monitoring/domain/services/HealthMeasureQueryService.java
@@ -4,6 +4,7 @@ import com.digitaldart.guardian.area.monitoring.domain.model.aggregates.HealthMe
 import com.digitaldart.guardian.area.monitoring.domain.model.queries.GetAllHealthMeasuresByDeviceRecordIdQuery;
 import com.digitaldart.guardian.area.monitoring.domain.model.queries.GetHealthMeasuresDailyAverageFromCurrentMonthByGuardianAreaDeviceRecordIdQuery;
 import com.digitaldart.guardian.area.monitoring.domain.model.valueobjects.HealthMeasureDailyAverage;
+
 import java.util.List;
 
 public interface HealthMeasureQueryService {
