d88e41f448222f9002fc3ec2e9d82ef8cd8711eb
steve <stev8855@gmail.com>
2024-11-18 23:09:26 -0500

    feat: Add missing import statement for UpdateHealthThresholdsCommand in HealthThresholdCommandService

--- src/main/java/com/digitaldart/guardian/area/monitoring/domain/services/HealthThresholdCommandService.java
+++ src/main/java/com/digitaldart/guardian/area/monitoring/domain/services/HealthThresholdCommandService.java
@@ -1,5 +1,6 @@
 package com.digitaldart.guardian.area.monitoring.domain.services;
 
+import com.digitaldart.guardian.area.monitoring.domain.model.commands.UpdateHealthThresholdsCommand;
 import com.digitaldart.guardian.area.monitoring.interfaces.rest.resource.DeviceHealthMeasureResource;
 
 public interface HealthThresholdCommandService {
