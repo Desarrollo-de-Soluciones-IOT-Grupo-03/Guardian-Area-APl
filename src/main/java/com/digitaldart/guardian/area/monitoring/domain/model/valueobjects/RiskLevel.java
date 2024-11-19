694b79705cec02c814d3a905f5193363c424d6a1
steve <stev8855@gmail.com>
2024-11-18 23:08:54 -0500

    feat: Add RiskLevel enum for activity risk assessment and update ActivityCommandService interface

---
+++ src/main/java/com/digitaldart/guardian/area/monitoring/domain/model/valueobjects/RiskLevel.java
@@ -0,0 +1,6 @@
+package com.digitaldart.guardian.area.monitoring.domain.model.valueobjects;
+
+public enum RiskLevel {
+    DANGER,
+    SAFE
+}
--- src/main/java/com/digitaldart/guardian/area/monitoring/domain/services/ActivityCommandService.java
+++ src/main/java/com/digitaldart/guardian/area/monitoring/domain/services/ActivityCommandService.java
@@ -2,6 +2,7 @@ package com.digitaldart.guardian.area.monitoring.domain.services;
 
 import com.digitaldart.guardian.area.monitoring.domain.model.aggregates.Activity;
 import com.digitaldart.guardian.area.monitoring.domain.model.commands.CreateActivityCommand;
+
 import java.util.Optional;
 
 public interface ActivityCommandService {
