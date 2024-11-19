78d9e0d953577ed543de8704b07968bd14779bb8
MaryJenn05 <u202120911@upc.edu.pe>
2024-11-18 22:06:14 -0500

    update GeoFence Repository and device command

--- src/main/java/com/digitaldart/guardian/area/monitoring/infrastructure/persistence/jpa/repositories/GeoFenceRepository.java
+++ src/main/java/com/digitaldart/guardian/area/monitoring/infrastructure/persistence/jpa/repositories/GeoFenceRepository.java
@@ -10,8 +10,8 @@ import java.util.Optional;
 
 @Repository
 public interface GeoFenceRepository extends JpaRepository<GeoFence, Long> {
-    boolean existsByNameAndGuardianAreaDeviceRecordId(String name, GuardianAreaDeviceRecordId guardianAreaDeviceRecordId);
-    Optional<GeoFence> findByGuardianAreaDeviceRecordId(GuardianAreaDeviceRecordId guardianAreaDeviceRecordId);
-    Optional<GeoFence> findByNameAndGuardianAreaDeviceRecordId(String name, GuardianAreaDeviceRecordId guardianAreaDeviceRecordId);
-    List<GeoFence> findAllByGuardianAreaDeviceRecordId(GuardianAreaDeviceRecordId guardianAreaDeviceRecordId);
+  boolean existsByNameAndGuardianAreaDeviceRecordId(String name, GuardianAreaDeviceRecordId guardianAreaDeviceRecordId);
+  Optional<GeoFence> findByGuardianAreaDeviceRecordId(GuardianAreaDeviceRecordId guardianAreaDeviceRecordId);
+  Optional<GeoFence> findByNameAndGuardianAreaDeviceRecordId(String name, GuardianAreaDeviceRecordId guardianAreaDeviceRecordId);
+  List<GeoFence> findAllByGuardianAreaDeviceRecordId(GuardianAreaDeviceRecordId guardianAreaDeviceRecordId);
 }
